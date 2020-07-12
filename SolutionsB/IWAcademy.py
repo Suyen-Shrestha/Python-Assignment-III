a6e9c336edaaf411c7e6875420f5f5f136620d59
Suyen Shrestha <suyencrestha@gmail.com>
2020-07-12 18:17:52 +0545

    Completed SolutionsB

--- SolutionsB/IWAcademy.py
+++ SolutionsB/IWAcademy.py
@@ -11,31 +11,120 @@ class Student:
     def enroll(self):
         name = input('Enter your fullname: ')
         address =input('Enter your address: ')
-        installment = int(input('Please enter Rs. 10000 for the installment: '))
+        installment = int(input('Please enter Rs. 10000 for the first installment: \n'))
         if installment < 10000:
             print('Please make sure that installment is at least Rs.10000!\n')
+            print('Enroll cancelled try again!!!\n')
         else:
             unpaid = 20000 - installment
             status = 'enrolled'
+            all = []
             student_detail = [name, address, installment, unpaid, status]
-            with open('student_infos.csv', 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(student_detail)
+            with open('student_infos.csv', 'r') as Input, open('student_infos.csv', 'a',newline='') as Output:
+                reader = csv.reader(Input, delimiter = ',')
+                writer = csv.writer(Output, delimiter = ',')
+                count = len(list(reader))
+                if count == 0:
+                    writer.writerow(['ID','Student Name', 'Address', 'Paid Amount', 'Unpaid Amount', 'Returned Amount', 'Course Status'])
+                    student_detail.insert(0,count+1)
+                    writer.writerow(student_detail)
+                else:    
+                    student_detail.insert(0,count)  #for adding student's id by incrementing total count of rows in csv file by 1 each time a new student is added.2
+                    writer.writerow(student_detail)
             print('Congratulations!! you are successfully enrolled!\n')
 
     def displayStudents(self):
         with open('student_infos.csv', 'r') as file:
             reader = csv.reader(file, skipinitialspace=True)
-            headers = ['Student Name', 'Address', 'Paid Amount', 'Unpaid Amount', 'Course Status']
+            
             for row in reader:
                 self.student_details.append(row)
+                print(row[0])
             print('Below are the details of enrolled students:\n')    
-            print(f'{tabulate(self.student_details,headers)}')
+            print(f'{tabulate(self.student_details,headers="firstrow")}')
             print('\n')
+            # else:
+            #     print('No students enrolled currently!!\n')    
 
-    # def updateStudent(self, name, address, paid):
+    def updateStudent(self, id):
+        name = input('Enter the updated name: ')
+        address = input('Enter the updated address: ')
+        paid = input('Enter the updated paid amount: ')
+        unpaid = str(20000 - int(paid))
+        returned = 0
+        status = input('Enter the updated Course status: ')
+
+        lines = list() # list for maintaing entire rows after updation of single row.
+        up_dict = {} # dictionary for maintaining the key value pairs of updated csv row.
+       
+        with open('student_infos.csv', 'r') as readFile:
+            reader = csv.DictReader(readFile)
+            for row in reader:
+                if row['ID'] == id:  # checks if the id value in the row is equal to id passed by user.
+                    up_dict['ID'] = row['ID']
+                    up_dict['Student Name'] = name
+                    up_dict['Address'] = address
+                    up_dict['Paid Amount'] = paid
+                    up_dict['Unpaid Amount'] = unpaid
+                    up_dict['Returned Amount'] = returned
+                    up_dict['Course Status'] = 'enrolled' #status is only changed to completed from option 6.
+                    lines.append(up_dict) # appending the updated dictionary to the lines list.
+                else:
+                    lines.append({k : v for k , v in row.items()})
+                                  
+        with open('student_infos.csv', 'w', newline='') as writeFile:
+            fields = lines[0].keys()
+            writer = csv.DictWriter(writeFile, fieldnames=fields)
+            writer.writeheader() 
+            writer.writerows(lines)
+            print('Student updated successfully!!\n')
+                        
 
 
+    def deleteStudent(self, id):
+        lines = list()
+        with open('student_infos.csv', 'r') as readFile:
+            reader = csv.reader(readFile)
+            for row in reader:
+                lines.append(row)
+                for field in row:
+                    if field == id:
+                        lines.remove(row)
+        with open('student_infos.csv', 'w', newline='') as writeFile:
+            writer = csv.writer(writeFile)  
+            writer.writerows(lines)
+            print('Student deleted successfully!!\n')     
+            
+
+
+    def graduateStudent(self, id):
+        
+        lines = list() 
+        up_dict = {} 
+       
+        with open('student_infos.csv', 'r') as readFile:
+            reader = csv.DictReader(readFile)
+            for row in reader:
+                if row['ID'] == id: 
+                    up_dict['ID'] = row['ID']
+                    up_dict['Student Name'] = row['Student Name']
+                    up_dict['Address'] = row['Address']
+                    up_dict['Paid Amount'] = row['Paid Amount']
+                    up_dict['Unpaid Amount'] = row['Unpaid Amount']
+                    up_dict['Returned Amount'] = '20000'
+                    up_dict['Course Status'] = 'completed' #status is changed to completed after student's graduation.
+                    lines.append(up_dict) 
+                else:
+                    lines.append({k : v for k , v in row.items()})
+            print(lines)        
+                                  
+        with open('student_infos.csv', 'w', newline='') as writeFile:
+            fields = lines[0].keys()
+            writer = csv.DictWriter(writeFile, fieldnames=fields)
+            writer.writeheader() 
+            writer.writerows(lines)
+            print('Student graduated successfully!!\n')
+                        
         
 
 
@@ -96,4 +185,18 @@ while ch != 8:
     elif ch == '2':
         st.enroll()
     elif ch == '3':
-        st.displayStudents()     
+        st.displayStudents()
+    elif ch == '4':
+        id = input('Enter a student\'s ID to update: ')
+        st.updateStudent(id)    
+    elif ch == '5':
+        id = input('Enter a student\'s ID to delete: ')
+        st.deleteStudent(id) 
+    elif ch == '6':
+        num = input("\tEnter The students id who have completed the course : ")
+        st.graduateStudent(num)
+    elif ch == '7':
+        print("\tThanks for visiting Insight Workshop Acadamey Program!")
+        break
+    else :
+        print("Invalid choice")          
